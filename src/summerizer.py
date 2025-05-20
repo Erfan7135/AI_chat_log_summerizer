bdcce097415a547c2286a83711d42fecf8ea850d
Erfan7135 <1805080@ugrad.cse.buet.ac.bd>
2025-05-20 23:53:50 +0600

    Chat Log (single line content) parsed & stored in Message Structure

--- src/summerizer.py
+++ src/summerizer.py
@@ -1,18 +1,33 @@
 
 
+def parse_chat(chat_path):
 
-def main():
-    chat = 'logs/chat1.txt'
+    # messages = []
+    messages = []
     try:
-        with open(chat, 'r', encoding='utf-8') as file:
-            content = file.read()
-            print(content)
+        with open(chat_path, 'r', encoding='utf-8') as file:
+            lines = file.readlines()
+            # print(lines)
+            # doesn't work if multiple lines in content
+            for line in lines:
+                speaker, content = line.split(':', 1)
+                messages.append({
+                    'speaker': speaker.strip(),
+                    'content': content.strip()
+                })
+            return messages
     except FileNotFoundError:
-        print(f"File {chat} not found.")
-        return
+        print(f"File {chat_path} not found.")
+        return []
     except Exception as e:
         print(f"An error occurred: {e}")
-        return
+        return []
+
+
+def main():
+    chat_path = 'logs/chat1.txt'
+    messages = parse_chat(chat_path)
+    print(messages)
 
 if __name__ == "__main__":
-     main()
+    main()
